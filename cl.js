efe51dff94ae77d0bc70d5d6766e1e5b5bac7ff9
student <aborjrcl@gmail.com>
2019-04-23 10:01:53 -0500

    noice

---
+++ cl.js
@@ -0,0 +1,7 @@
+var best = "sara lee";
+
+var cl_choice = best;
+
+if (cl_choice===best) {
+	console.log(you have got the bread);
+}
---
+++ jspractice.js
@@ -0,0 +1,180 @@
+// var bread = math.floor(math.random)
+
+
+
+
+
+
+
+// var user_answer = prompt("big daddy is headed to Wegman's, what is your favorite");
+
+// console.log("your favorite bread is kroger bread");
+
+
+// var cups = 1
+
+// if(cups < 3){
+    // console.log("Yes. I'll take another cup of coffee!");
+// } else {
+    // console.log("I think I'm okay for now.");
+// }
+
+
+// var cupofcoffee = prompt("how many cups of coffee have you drank today?");
+// if (cupofcoffee <3){
+	// console.log("here is a cup of coffee");
+// }
+
+// else {
+// 	// console.log("no more coffee for you");
+// }
+
+
+// var temp = 90;
+// console.log("The temp is " + temp + " degrees ");
+// if (temp > 80 && precipitation === false) {
+	// console.log("Time to Swim");
+// }
+// var precipitation = false;
+
+
+
+
+
+// for (var x = 1; x <= 10; x++) {
+	// console.log(x + 1);
+// }
+
+// for (var x = 99; x >= 0; x--) {
+	// console.log (x + " bottles of beer on the wall");
+	// if (x === 0 ) {
+		// console.log ( "we need more shots");
+	// }
+	// break;
+	// }
+
+
+
+//while loop
+// var correctAnser = prompt("Do you want to build a snowman?");
+// while (correctAnser != "yes") {
+	// var correctAnser = prompt("Do you want to build a snowman?");
+// }	
+
+
+// THIS IS THE ROCK, PAPER, Scissors GAME!!!!!!!!!!!
+//  var big_daddy = prompt("big daddy wants to play Rocky, Give me Paper, or Get Cut with Scissors");
+
+// var c = "Rocky";
+// var d = "Give me Paper";
+// var e = " Get Cut with Scissors";
+// var comp = window['cde'.charAt(Math.floor(Math.random()*3))];
+// alert('The computer chose: ' + comp);
+
+// var compare = function (comp,big_daddy,cake)
+//  if(comp == big_daddy)
+
+//  {
+//         console.log("its a tie");
+//     }
+//     else if((comp == "Rocky" && big_daddy == "Get Cut with Scissors" && "Give me Paper") || (comp == "Give me Paper" && big_daddy == "Rocky" && "Get Cut with Scissors") || (comp == "Get Cut with Scissors" && big_daddy =="Give me Paper" && "Rocky"))
+//     {
+//         console.log("Damn Computer wins again!!");
+//     }
+//     else if((big_daddy == "Rocky" && comp == "Get Cut with Scissors" && "Give me Paper") || (big_daddy == "Give me Paper" && comp == "Rocky" && "Get Cut with Scissors") || (big_daddy == "Get Cut with Scissors" && comp =="Give me Paper" && "Rocky"))
+//     {
+//         console.log("Big Zaddy!! Wins!!");
+//     }
+
+
+
+
+
+// var compare = function(computerChoice,playerChoice) {
+//     if (computerChoice === playerChoice)
+//     {
+//         console.log("its a tie");
+//     }
+//     else if((computerChoice == "rock" && playerChoice == "scissors") || (computerChoice == "paper" && playerChoice == "rock") || (computerChoice == "scissors" && playerChoice =="paper"))
+//     {
+//         console.log("computer wins");
+//     }
+//     else if((playerChoice == "rock" && computerChoice == "scissors") || (playerChoice == "paper" && computerChoice == "rock") || (playerChoice == "scissors" && computerChoice =="paper"))
+//     {
+//         console.log("player wins");
+//     }
+// }
+
+
+
+
+
+
+
+//Java basics MDN: 
+
+//  var iceCream = 'vanilla';
+// if (iceCream === 'chocolate') {
+//   alert('Yay, I love chocolate ice cream!');    
+// } else {
+//   alert('Awwww, but chocolate is my favorite...');    
+// }
+
+
+// function multiply(num1,num2) {
+//   var result = num1 * num2;
+// multiply(4, 7);
+// multiply(20, 20);
+// multiply(0.5, 3);
+// return (result);
+
+// }
+
+
+// Click function 
+// document.querySelector('html').onclick = function() {
+//     alert('Ouch! Stop poking me!');
+// }
+
+
+
+// var myImage = document.querySelector('img');
+
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/firefox.png') {
+//       myImage.setAttribute ('src','/c/Users/Student/desktop/images/firefox_2.png');
+//     } else {
+//       myImage.setAttribute ('src','/c/Users/Student/desktop/images/firefox.png');
+//     }
+// }
+
+
+var kitchenlist = ["pop tarts", "ramen", "noodles", "chips", "salsa", "coffee"];
+// kitchenlist [6] = "fruit loops"
+// kitchenlist [5] = "fair trade coffee"
+// kitchenlist [3,4] = "rice","beans";
+
+//kitchen.length=7,i starts count from 0, continue running until i is greater than 7, i++ starts running on 2nd loop
+//keep running this for loop until length of kitchenlist is greater than "i" then stop;
+for (var i = 0; i < kitchenlist.length; i++) {
+	console.log(kitchenlist[i]);
+}
+
+// console.log(kitchenlist.length);//counts how many strings are inside the array
+// console.log(kitchenlist);
+
+
+
+// kitchenlist.pop();//deletes strings in the array from the back first
+// kitchenlist.push();//creates new space at the end of the array and push it to the back
+
+
+// JAVA ARRAYS (DAY2)
+
+
+
+
+
+
+
