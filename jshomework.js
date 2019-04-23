f8aa3541efa4f501e8e034df16d312a0e7d26e07
student <aborjrcl@gmail.com>
2019-04-23 16:36:36 -0500

    exercise 2 javascript

--- jshomework.js
+++ jshomework.js
@@ -1,38 +1,37 @@
-var kitchenlist = ["pop tarts", "ramen", "noodles", "chips", "salsa", "coffee"];
+// var kitchenlist = ["pop tarts", "ramen", "noodles", "chips", "salsa", "coffee"];
 //kitchenlist [6] = "fruit loops"//my first answer without trying to use push 
-kitchenlist [5] = "fair trade coffee"
+// kitchenlist [5] = "fair trade coffee"
 
-kitchenlist.push('fruit loops');
+// kitchenlist.push('fruit loops');
 
 //starts at 3,take away 2 from the starting point 3, then add the strings you input afterwards sssooo dope bro
- kitchenlist.splice (3,2, "rice","beans");
- console.log(kitchenlist);
+//  kitchenlist.splice (3,2, "rice","beans");
+//  console.log(kitchenlist);
 
-var walmartkart = [];
-kitchenlist.pop();
-console.log(kitchenlist);
+// var walmartkart = [];
+// kitchenlist.pop();
+// console.log(kitchenlist);
 
 
-walmartkart.push(kitchenlist.pop());
+// walmartkart.push(kitchenlist.pop());
 // walmartkart.push('fruit loops');
 // console.log(walmartkart);
 
 // kitchenlist.shift();
 // console.log(kitchenlist);
- walmartkart.push("pop tarts");
- console.log(walmartkart);
+ // walmartkart.push("pop tarts");
+ // console.log(walmartkart);
 
-walmartkart.push(kitchenlist.pop(kitchenlist.reverse));
+// walmartkart.push(kitchenlist.pop(kitchenlist.reverse));
 
- while (kitchenlist.length !=0)
- {
- 	walmartkart.push(kitchenlist.pop());
- 	console.log(walmartkart);
- }
+//  while (kitchenlist.length !=0)
+//  {
+//  	walmartkart.push(kitchenlist.pop());
+//  	console.log(walmartkart);
+//  }
 
 
-walmartkart.reverse(walmartkart.sort());
-
+// walmartkart.reverse(walmartkart.sort());
 
 
 
@@ -56,3 +55,103 @@ walmartkart.reverse(walmartkart.sort());
 
 // kitchenlist.pop();//deletes strings in the array from the back first
 // kitchenlist.push();//creates new space at the end of the array and push it to the back
+
+
+//exercise2: Objects
+
+
+//exercise2: Practice
+// var course = {
+// 	name: 'JavaScript Applications',
+// 	awesome: true,
+// 	teachers: ['Assaf', 'Shane'],
+// 	students: [
+// 		{
+// 			name: 'Steve',
+// 			computer: {
+// 				OS: 'Linux',
+// 				type: 'laptop'
+// 			}
+// 		}
+// 	]
+// };
+
+// console.log(course.students[0].computer.OS);
+
+// course.name = "super duper class";
+// console.log(course.name);
+
+// course.fun = true; //add a property
+// delete course.name; //remove one
+// console.log(course);
+
+// exercise2 code
+var course = {
+	name: 'JavaScript Applications',
+	awesome: true,
+	teachers: ['Assaf', 'Shane'],
+	students: [
+		{
+			name: 'Steve',
+			computer: {
+				OS: 'Linux',
+				type: 'laptop'
+			}
+		},
+		{
+			name: 'Katy',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		},
+		{
+			name: 'Chuck',
+			computer: {
+				OS: 'OSX',
+				type: 'macbook'
+			}
+		}
+
+	],
+	preReqs : {
+		skills : ['html', 'css', 'git'],
+		equipment: {
+			laptop: true,
+			OSOptions: ['linux', 'osx']
+		}
+	}
+
+ };
+
+
+//exercise2 homework
+console.log(course.name);
+console.log(course.teachers[1]);
+console.log(course.students[0].name);
+console.log(course.students[1].computer.type);
+console.log(course.preReqs.equipment.OSOptions[1]);
+console.log(course.preReqs.equipment);
+console.log(course.preReqs.equipment.OSOptions.join(' or '));
+
+var OS = course.students.filter((stud) => {
+  return stud.computer.OS === 'OSX';
+ });
+console.log(OS);
+
+
+
+
+
+//class notesc
+// var name_of_object = {
+// 	key: value
+// }
+
+//redeclare/reassigning a var is important terms for coding
+
+
+// console.log(dog.name,dog.coat,dog.siblings)
+
+
+
